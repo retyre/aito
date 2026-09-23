88133a8ffe6f31f501527e309969aabd0349f6df
Re Tyre <grexer@gmail.com>
2026-09-23 18:47:40 -0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-add_lunch_combo fox_aito-eng
+# add_lunch_combo is obsolete - COMMON_LUNCH_CHOICES in AndroidProducts.mk is used instead
 export FOX_BUILD_DEVICE=aito
 export LC_ALL=C
 export ALLOW_MISSING_DEPENDENCIES=true
