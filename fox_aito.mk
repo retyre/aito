62d45f052808f24f32d620f27ac90aa65810218e
Re Tyre <grexer@gmail.com>
2026-09-23 19:28:42 -0400

    Update fox_aito.mk

--- fox_aito.mk
+++ fox_aito.mk
@@ -1,7 +1,7 @@
 # OrangeFox product for aito
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, vendor/fox/config/common.mk)
+$(call inherit-product-if-exists, vendor/fox/config/common.mk)
 $(call inherit-product, device/motorola/aito/device.mk)
 
 PRODUCT_DEVICE := aito
